795c37dc4fc051d6394f6e9cbcd6d2da9a7213f2
yannor <yannick.servranckx@hotmail.com>
2017-08-18 01:49:15 +0200

    Src files added

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,36 @@
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
+
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+
+
+
+ const routes: Routes = [
+    {
+        path: 'heroes',
+        component: HeroesComponent
+    },
+    {
+        path: 'dashboard',
+        component: DashboardComponent
+    },
+    {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+    },
+    {
+        path: 'detail/:id',
+        component: HeroDetailComponent
+    }
+];
+
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule {}
